c412a1ed6a9cebdaded0db5f8f9ebe3e5dfb5205
Zuppah1 <mantvydas.rinkunas@gmail.com>
2025-02-17 18:21:58 +0200

    Created groups, rewrote loop to use groups

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,)
 
     while True:
@@ -15,8 +21,9 @@ def main():
                 return
 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for entity in drawable:
+            entity.draw(screen)
         pygame.display.flip()
 
         # limit FPS to 60
